9af30869c3bee1fc3ae71f757e037a3dfecb2b21
tiernan1979 <76110362+tiernan1979@users.noreply.github.com>
2025-05-10 09:26:48 +0800

    Create __init__.py

---
+++ custom_components/exchange_calendar/__init__.py
@@ -0,0 +1,10 @@
+{
+  "domain": "exchange_calendar",
+  "name": "Exchange Calendar",
+  "config_flow": true,
+  "documentation": "https://www.home-assistant.io/integrations/calendar",
+  "requirements": ["exchangelib==5.4.2"],
+  "codeowners": [],
+  "iot_class": "cloud_polling",
+  "version": "1.0.0"
+}
