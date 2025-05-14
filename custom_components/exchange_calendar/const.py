5c1399bc31782ccdfd8d8595bd79f6dd6c35faef
tiernan1979 <76110362+tiernan1979@users.noreply.github.com>
2025-05-14 08:05:22 +0800

    Create const.py

---
+++ custom_components/exchange_calendar/const.py
@@ -0,0 +1,13 @@
+DOMAIN = "exchange_calendar"
+
+CONF_EMAIL = "email"
+CONF_PASSWORD = "password"
+CONF_SERVER = "server"
+CONF_TIMEZONE = "timezone"
+CONF_AUTH_TYPE = "auth_type"
+
+AUTH_TYPES = ["NTLM", "basic"]
+
+SERVICE_CREATE_EVENT = "create_event"
+SERVICE_DELETE_EVENT = "delete_event"
+SERVICE_SEARCH_EVENT = "search_event"
